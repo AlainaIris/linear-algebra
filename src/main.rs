2d6af64e595bf567701762c23828e02bb6c30e48
Alaina Iris <alainairis@proton.me>
2025-01-24 02:48:15 -0500

    Added row operations

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,7 @@ fn m4() -> Matrix {
         values: vec![
             vec![4.0],
             vec![3.0],
-            vec![5.0]
+            vec![5.0],
         ],
         cols: 1,
         rows: 3
@@ -75,19 +75,31 @@ mod tests {
     }
 
     #[test]
+    fn can_multiply() {
+        let m1 = m1();
+        let m2 = m2();
+        let m3 = m3();
+        let m4 = m4();
+        assert!(!Matrix::can_multiply(&m4, &m3));
+        assert!(!Matrix::can_multiply(&m2, &m3));
+        assert!(Matrix::can_multiply(&m1, &m2));
+        assert!(Matrix::can_multiply(&m3, &m4));
+    }
+
+    #[test]
     fn mult_test() {
         let m1 = m1();
         let m2 = m2();
         let m3 = m3();
         let m4 = m4();
-        assert!(m1.multiply(&m2).expect("").equals(&Matrix { values: vec![
+        assert!(m1.multiply(&m2).expect("Something went wrong with this multiplication").equals(&Matrix { values: vec![
             vec![5.0, 1.0],
             vec![5.0, 3.0]
         ],
             cols: 2,
             rows: 2
         }));
-        assert!(m3.multiply(&m4).expect("").equals(&Matrix { values: vec![
+        assert!(m3.multiply(&m4).expect("Something went wrong with this multiplicaton").equals(&Matrix { values: vec![
             vec![65.0],
             vec![11.0],
             vec![-21.0],
@@ -95,5 +107,55 @@ mod tests {
         cols: 1,
         rows: 3,
         }));
+    }
+
+    #[test]
+    fn row_scale_test() {
+        let mut m1 = m1();
+        let mut m2 = m2();
+        m1.apply_row_scalar(0, 2.0);
+        m2.apply_row_scalar(1, -1.0);
+        assert!(m1.equals(&Matrix {values: vec![
+            vec![4.0, 6.0],
+            vec![4.0, 5.0]
+        ], rows: 2, cols: 2}));
+        assert!(m2.equals(&Matrix {values: vec![
+            vec![-5.0, 2.0],
+            vec![-5.0, 1.0]
+        ], rows: 2, cols: 2}));
+    }
+
+    #[test]
+    fn row_swap_test() {
+        let mut m1 = m1();
+        let mut m2 = m2();
+        m1.apply_row_swap(0, 1);
+        m2.apply_row_swap(1, 0);
+
+        assert!(m1.equals(&Matrix {values: vec![
+            vec![4.0, 5.0],
+            vec![2.0, 3.0]
+        ], rows: 2, cols: 2}));
+        assert!(m2.equals(&Matrix {values: vec![
+            vec![5.0, -1.0],
+            vec![-5.0, 2.0]
+        ], rows: 2, cols: 2}));
+    }
+
+    #[test]
+    fn row_addition_test() {
+        let mut m1 = m1();
+        let mut m2 = m2();
+        m1.apply_row_addition(1, 0);
+        m2.apply_row_addition(0, 1);
+
+        assert!(m1.equals(&Matrix {values: vec![
+            vec![2.0, 3.0],
+            vec![6.0, 8.0]
+        ], rows: 2, cols: 2}));
+        assert!(m2.equals(&Matrix {values: vec![
+            vec![0.0, 1.0],
+            vec![5.0, -1.0]
+        ], rows: 2, cols: 2}));
     }
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 
+/// Matrix format
 #[derive(Clone)]
 pub struct Matrix {
     pub values: Vec<Vec<f64>>,
@@ -7,15 +8,48 @@ pub struct Matrix {
     pub cols: usize,
 }
 
+/// # Matrix Algebra Functions
 impl Matrix {
+    /// Checks if the dimensions of two matricies are the same
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let m1 = Matrix {values: ..., rows: 2, cols: 3};
+    /// let m2 = Matrix {values: ..., rows: 2, cols: 3};
+    ///
+    /// assert!(Matrix::same_dimensions(&m1, &m2));
+    /// ```
     pub fn same_dimensions(m1: &Matrix, m2: &Matrix) -> bool {
         m1.rows == m2.rows && m1.cols == m2.cols
     }
 
+    /// Checks if two matricies can be multiplied (in the order given)
+    ///
+    /// # Examples
+    /// 
+    /// ```
+    /// let m1 = Matrix {values..., rows: 3, cols: 3};
+    /// let m2 = Matrix {values..., rows: 1, cols: 3};
+    ///
+    /// assert!(!Matrix::can_multiply(&m1, &m2));
+    /// assert!(Matrix::can_multiply(&m2, &m1));
+    /// ```
     pub fn can_multiply(m1: &Matrix, m2: &Matrix) -> bool {
         m1.cols == m2.rows
     }
 
+    /// Checks if two matricies are the same size and share all the
+    /// same entries
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let m1 = Matrix {values: vec![vec![2.0, 3.0], vec![4.0, 3.0]], rows: 2, cols: 2};
+    /// let m2 = Matrix {values: vec![vec![2.0, 3.0], vec![4.0, 3.0]], rows: 2, cols: 2};
+    ///
+    /// assert!(m1.equals(&m2));
+    /// ```
     pub fn equals(&self, m: &Matrix) -> bool {
         if !Self::same_dimensions(&self, m) {
             return false;
@@ -30,6 +64,17 @@ impl Matrix {
         return true;
     }
 
+    /// Add two matricies
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let m1 = Matrix ...; // Representing [2, 3, 4]
+    /// let m2 = Matrix ...; // Representing [4, 5, 9]
+    /// let m3 = Matrix ...; // Representing [6, 8, 13]
+    /// 
+    /// assert!(m1.add(&m2).expect("Unable to add").equals(&m3));
+    /// ```
     pub fn add(&self, m: &Matrix) -> Result<Matrix, Box<dyn Error>> {
         if !Self::same_dimensions(&self, m) {
             return Err("Invalid Addition".into());
@@ -44,6 +89,17 @@ impl Matrix {
         }
     }
 
+    /// Multiply two matricies
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let m1 = Matrix ...; // Representing [1, 4, 2] (1 x 3)
+    /// let m2 = Matrix ...; // Representing [[5], [8], [1]] (3 x 1)
+    /// let m3 = Matrix ...; // Representing [39] (1 x 1)
+    ///
+    /// assert!(m1.multiply(&m2).expect("Unable to multiply").equals(&m3));
+    /// ```
     pub fn multiply(&self, m: &Matrix) -> Result<Matrix, Box<dyn Error>> {
         if !Self::can_multiply(&self, m) {
             return Err("Invalid multiplication".into());
@@ -66,6 +122,7 @@ impl Matrix {
         }
     }
 
+    /// Pretty print for matrix
     pub fn print(&self) {
         let mut text = String::new();
         for i in &self.values {
@@ -76,5 +133,57 @@ impl Matrix {
             text.push_str("\n");
         }
         print!("{}", text);
+    }
+}
+
+/// # Row Operations
+impl Matrix {
+    /// Apply a scalar to a row
+    ///
+    /// # Examples
+    /// 
+    /// ```
+    /// let mut m1 = Matrix ... // Representing [[1, 2], [3, 4]] (2 x 2)
+    /// m1.apply_row_scalar(0, 3.0);
+    /// let m2 = Matrix ... // Representing [[3, 6], [3, 4]] (2 x 2)
+    ///
+    /// assert!(m1.equals(&m2));
+    /// ```
+    pub fn apply_row_scalar(&mut self, row: usize, scalar: f64) {
+        for i in &mut self.values[row] {
+            *i *= scalar;
+        }
+    }
+
+    /// Apply a row swap
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut m1 = Matrix ... // Representing [[0, 0, 0], [1, 1, 1]] (2 x 3)
+    /// let m2 = Matrix ... // Representing [[1, 1, 1], [0, 0, 0]] (2 x 3)
+    /// m1.apply_row_swap(0, 1);
+    ///
+    /// assert!(m1.equals(&m2));
+    /// ```
+    pub fn apply_row_swap(&mut self, row1: usize, row2: usize) {
+        self.values.swap(row1, row2);
+    }
+
+    /// Apply a row addition
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut m1 = Matrix ... // Representing [[2, 3], [4, 5]] (2 x 2)
+    /// let m2 = Matrix ... // Representing [[2, 3], [6, 8]] (2 x 2)
+    /// m1.apply_row_addition(1, 0);
+    ///
+    /// assert!(m1.equals(&m2));
+    /// ```
+    pub fn apply_row_addition(&mut self, row: usize, row_addend: usize) {
+        for i in 0..self.cols {
+            self.values[row][i] += self.values[row_addend][i];
+        }
     }
 }
